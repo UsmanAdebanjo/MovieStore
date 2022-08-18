75a564e534f0426a726d00fcad5c25f5fad27ba9
Usman Adebanjo <oluwatobilobalayeuthman@gmail.com>
2022-08-18 22:27:29 +0100

    modified customer

---
+++ MovieStore/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieStore.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public MembershipType MembershipType { get; set; }
+
+        [Required]
+        public int MembershipTypeId { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+
+    }
+}
