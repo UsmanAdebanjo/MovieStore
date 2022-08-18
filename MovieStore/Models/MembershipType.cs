b9d178a3e0f0910851d81c876b9aa2454ac2a9a9
Usman Adebanjo <oluwatobilobalayeuthman@gmail.com>
2022-08-18 22:38:39 +0100

    membershiptype

---
+++ MovieStore/Models/MembershipType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieStore.Models
+{
+    public class MembershipType
+    {
+        public int Id { get; set; }
+
+        [Required]
+
+        [StringLength(100)]
+        public string Name { get; set; }
+        public int SignUpFee { get; set; }
+        public int DiscountRate { get; set; }
+
+        public byte DurationInMonth { get; set; }
+    }
+}
